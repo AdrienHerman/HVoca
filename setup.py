271e1726001557fc723632f9fd66f9a04ea5e007
Adrien Herman <herman1.adrien@gmail.com>
2021-04-14 00:07:57 +0200

    Update setup.py
    Win32GUI

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 from cx_Freeze import setup, Executable
 
-platform = "Win32GUI"
-
 options = {
   "include_files" : ["icon/"]
 }
 
 setup(
-  base = platform,
   name = "HVoca",
   version = "Beta 1.1",
   description = "Logiciel d'apprentissage du vocabulaire à partir de liste créées par l'utilisateur.",
   author = "Adrien Herman",
   options = {"build_exe" : options},
-  executables = [Executable("main.py")]
+  executables = [Executable("main.py", build="Win32GUI")]
 )
