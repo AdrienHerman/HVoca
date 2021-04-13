ff111a3a37b0ccf7a45c30039d752da433f570f9
AdrienHerman <herman1.adrien@gmail.com>
2021-04-14 00:02:15 +0200

    Modification du setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from cx_Freeze import setup, Executable
 
+platform = "Win32GUI"
+
 options = {
   "include_files" : ["icon/"]
 }
 
 setup(
+  base = platform,
   name = "HVoca",
   version = "Beta 1.1",
   description = "Logiciel d'apprentissage du vocabulaire à partir de liste créées par l'utilisateur.",
